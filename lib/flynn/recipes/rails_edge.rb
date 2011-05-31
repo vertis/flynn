ffd6645483ef7b0628a803ee33ac8faf0a68b702
Luke Chadwick <luke.a.chadwick@gmail.com>
2011-06-01 00:03:44 +1000

    Run bundle install again in rails_edge app (there must be a better way)

--- lib/flynn/recipes/rails_edge.rb
+++ lib/flynn/recipes/rails_edge.rb
@@ -6,17 +6,28 @@ module Flynn
       def create(app_name, options=[])
         #disable_rvmrc
         rails_base = "#{ENV['HOME']}/.flynn/rails_edge"
+        rails_src = "#{ENV['HOME']}/.flynn/rails_src"
         RVM.gemset_create app_name
         RVM.gemset_use! app_name
 
         FileUtils.mkdir_p rails_base
         File.open("#{ENV['HOME']}/.flynn/rails_edge/Gemfile", 'w') {|f| f.puts gemfile }
-
+        if File.directory?(rails_src)
+          inside rails_src do
+            run("git pull")
+          end
+        else
+          run("git clone git://github.com/rails/rails.git #{rails_src}")
+        end
         bundle_cmd = "bundle install --gemfile #{rails_base}/Gemfile --binstubs"
         puts "Creating '#{app_name}' running edge rails (this could take a while)"
         run("rvm gemset use #{app_name} && #{bundle_cmd}")
         run("rvm gemset use #{app_name} && #{rails_base}/bin/rails new #{app_name} --dev #{options.join(" ")}")
         create_project_rvmrc(app_name)
+        # TODO: There must be a better way than having to call this a second time
+        inside app_name do
+          run "bundle install"
+        end
         #restore_rvmrc
       end
 
@@ -27,7 +38,7 @@ module Flynn
 source "http://rubygems.org"
 
 gem 'rake', '>= 0.9.0'
-gem 'rails', :git => 'git://github.com/rails/rails.git'
+gem 'rails', :git => "#{ENV['HOME']}/.flynn/rails_src"
           EOS
         end
     end
